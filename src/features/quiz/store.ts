2d9c0c56f93fd57fb58ad9c98aaf1a4d8d459697
kujo205 <kuc8301@gmail.com>
2026-02-04 14:29:42 +0200

    feat: added localization func

--- src/features/quiz/store.ts
+++ src/features/quiz/store.ts
@@ -1,42 +1,62 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import type { TQuiz } from "@/features/quiz/types-and-schemas";
+import type {
+  TLanguage,
+  TLocalizedString,
+  TQuiz,
+} from "@/features/quiz/types-and-schemas";
 
 type TQuizAnswer = string | string[];
 
 interface QuizResult {
   answers: Record<string, TQuizAnswer>;
   email: string;
+  lang: TLanguage;
 }
 
 interface QuizStore {
   results: Record<string, QuizResult>;
-
   setAnswer: (quizId: string, questionId: string, val: TQuizAnswer) => void;
   setEmail: (quizId: string, email: string) => void;
+  setLanguage: (quizId: string, lang: TLanguage) => void;
   resetQuiz: (quizId: string) => void;
 
-  // This is to store different quiz configurations
   configs: Record<string, TQuiz>;
   setConfig: (quizId: string, config: TQuiz) => void;
 }
+
+const DEFAULT_QUIZ_RESULT: QuizResult = {
+  answers: {},
+  email: "",
+  lang: "en", // English as default
+};
 
 export const useQuizStore = create<QuizStore>()(
   persist(
     (set) => ({
       results: {},
       configs: {},
+
       setConfig: (quizId, config) =>
         set((state) => ({
           configs: { ...state.configs, [quizId]: config },
         })),
 
+      setLanguage: (quizId, lang) =>
+        set((state) => ({
+          results: {
+            ...state.results,
+            [quizId]: {
+              // if no existing result, initialize with defaults
+              ...(state.results[quizId] || DEFAULT_QUIZ_RESULT),
+              lang,
+            },
+          },
+        })),
+
       setAnswer: (quizId, questionId, val) =>
         set((state) => {
-          const currentQuiz = state.results[quizId] || {
-            answers: {},
-            email: "",
-          };
+          const currentQuiz = state.results[quizId] || DEFAULT_QUIZ_RESULT;
           return {
             results: {
               ...state.results,
@@ -53,7 +73,7 @@ export const useQuizStore = create<QuizStore>()(
           results: {
             ...state.results,
             [quizId]: {
-              ...(state.results[quizId] || { answers: {}, email: "" }),
+              ...(state.results[quizId] || DEFAULT_QUIZ_RESULT),
               email,
             },
           },
@@ -71,3 +91,23 @@ export const useQuizStore = create<QuizStore>()(
     },
   ),
 );
+
+export const useQuizTranslation = (quizId: string) => {
+  const lang = useQuizStore((state) => state.results[quizId]?.lang || "en");
+
+  /**
+   * Func 't' (translate)
+   * takes in object of type TLocalizedString (напр. { en: "Hello", fr: "Salut" }) and returns
+   * the string in the current language, or falls back to English if the current language is not available.
+   */
+  const t = (localizedString?: TLocalizedString): string => {
+    if (!localizedString) {
+      console.error("Missing localized string for quiz:", quizId);
+      return "";
+    }
+
+    return localizedString[lang] || localizedString["en"] || "";
+  };
+
+  return { t, lang };
+};
