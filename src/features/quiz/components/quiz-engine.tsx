1f899b2439e2241f959cb3697755e334b9393a74
kujo205 <kuc8301@gmail.com>
2026-02-05 19:12:39 +0200

    fix: fixed ui layout in quiz engine

--- src/features/quiz/components/quiz-engine.tsx
+++ src/features/quiz/components/quiz-engine.tsx
@@ -2,16 +2,10 @@
 
 import { QuestionRenderer } from "@/features/quiz/components/question-renderer";
 import { QuizProgress } from "@/features/quiz/components/quiz-progress";
-import type {
-  TQuiz,
-  TQuizQuestion,
-  TQuizStaticStep,
-  TQuizStep,
-} from "@/features/quiz/types-and-schemas";
 
 export default function QuizEngine() {
   return (
-    <main className="max-w-xl min-h-screen flex flex-col mx-auto py-8 px-4">
+    <main className="max-w-xl h-dvh flex flex-col mx-auto py-8 px-4">
       <QuizProgress />
       <QuestionRenderer />
     </main>
