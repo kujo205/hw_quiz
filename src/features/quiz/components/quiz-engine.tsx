58305a90fd22f98c1332dc4b7c9a4e08f61a7fe1
kujo205 <kuc8301@gmail.com>
2026-02-06 20:50:30 +0200

    fix: check if need progress by step type

--- src/features/quiz/components/quiz-engine.tsx
+++ src/features/quiz/components/quiz-engine.tsx
@@ -7,9 +7,11 @@ import { getIsStaticStep } from "@/features/quiz/utils/get-is-static-step";
 import { cn } from "@/shared/utils/cn";
 
 export default function QuizEngine() {
-  const isStaticStep = useQuizStore((state) =>
-    getIsStaticStep(state.activeQuizStep),
-  );
+  const isStaticStep = useQuizStore((state) => {
+    const dataMode = state.getCurrentStepData()?.dataModel;
+    if (!dataMode) return false;
+    return getIsStaticStep(dataMode.type);
+  });
 
   return (
     <main className={cn("max-w-xl h-dvh flex flex-col mx-auto py-8 px-4")}>
