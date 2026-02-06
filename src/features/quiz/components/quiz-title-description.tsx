0561d09d21f77d64ed16d7f3ca7ccc1ee9381c07
kujo205 <kuc8301@gmail.com>
2026-02-06 13:10:36 +0200

    style: change title size on mobile

--- src/features/quiz/components/quiz-title-description.tsx
+++ src/features/quiz/components/quiz-title-description.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import { useQuizStore } from "@/features/quiz/store";
-import type { TLocalizedString } from "@/features/quiz/types-and-schemas";
-
 interface QuizTitleDescriptionProps {
   title: string;
   description?: string;
 }
 
 export function QuizTitleDescription(props: QuizTitleDescriptionProps) {
-  const t = useQuizStore((state) => state.t);
-
   return (
     <div className="text-center space-y-4 my-4">
-      <h1 className="text-white font-bold text-2xl">{props.title}</h1>
+      <h1 className="text-white font-bold text-xl md:text-2xl">
+        {props.title}
+      </h1>
       <p className="text-grey-200">{props.description}</p>
     </div>
   );
