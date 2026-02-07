5603a74dbbd80d6170a141f8f7f1c9c65f903722
kujo205 <kuc8301@gmail.com>
2026-02-07 22:15:25 +0200

    fix: fixed bg color in loading spinner

--- src/features/quiz/components/quiz-spinner.tsx
+++ src/features/quiz/components/quiz-spinner.tsx
@@ -2,7 +2,7 @@
 
 export const QuizSpinner = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-pink-dimmed">
+    <div className="flex items-center justify-center min-h-screen bg-background">
       <div className="relative w-12 h-12">
         {/* external ring*/}
         <div className="absolute inset-0 border-4 border-white/10 rounded-full"></div>
